fbaf236009b12e62db8caee578c62fb2e3ae9f19
ankitnarang-ai <ankitnarang2002@gmail.com>
2024-02-29 03:36:07 -0800

    cloudinary setup for storing images

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -85,5 +85,32 @@ userSchema.methods.isPasswordCorrect = async function (password: string): Promis
     throw error;
   }
 };
-
+// we use normal function instead of arrow function to get refrence of our object using "this" 
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign({
+    _id:this._id,
+    email:this.email,
+    username: this.username,
+    fullName: this.fullName
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+  }
+  )
+}
+// Refresh Token
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign({
+    _id:this._id,
+    email:this.email,
+    username: this.username,
+    fullName: this.fullName
+  },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+    expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+  }
+  )
+}
 export const User = mongoose.model("User", userSchema);
---
+++ src/utils/cloudinary.ts
@@ -0,0 +1,10 @@
+// first we store image in our local storage or local server than we upload it in cloudinary
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs";
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_CLOUD_SECRET
+});
+
